d2679ce7202de6fff6cd0f45136af25d3499366a
Shane Vitarana <shanev@gmail.com>
2026-01-14 10:31:01 -0500

    feat: Update autogit to 1.0.67

--- Formula/autogit.rb
+++ Formula/autogit.rb
@@ -1,12 +1,12 @@
 class Autogit < Formula
   desc "Blazing fast terminal-ui for git with AI superpowers"
   homepage "https://github.com/autogit-ai/autogit"
-  version "1.0.66"
+  version "1.0.67"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://assets.autogit.ai/releases/v1.0.66/autogit-v1.0.66-aarch64-apple-darwin.tar.gz"
-      sha256 "a513ab46f6496f2940b4acafc230311de5b7ff42c579de3abfde110ee23ea347"
+      url "https://assets.autogit.ai/releases/v1.0.67/autogit-v1.0.67-aarch64-apple-darwin.tar.gz"
+      sha256 "4eb90e3e1d59a5bd136147cfb4c86435bcf19a56ffb6008602d262700ee57067"
     end
   end
 
